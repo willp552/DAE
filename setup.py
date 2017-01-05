482322395948e43d469ba89332dc5c519732413c
William Price <willp552@gmail.com>
2017-01-05 14:04:22 +0000

    Added setup file to repo.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DAEpy',
+    version='0.1',
+    description='Collection of solvers for differential algebraic equations',
+    author='William Price',
+    author_email='wcp23@cam.ac.uk',
+    packages=find_packages()
+)
